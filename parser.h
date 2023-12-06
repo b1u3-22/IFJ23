fa505df404df4edf03189ee284f7e2554d7814a1
b1u3-22 <jiri.s.15243@gmail.com>
2023-12-06 01:59:18 +0100

    Added function call to generate if let ID

--- parser.h
+++ parser.h
@@ -96,6 +96,7 @@ enum Function {
     F_G_DEF_VAR,    // Generate variable definition
     F_G_SET_VAR,    // Generate variable assigment
     F_G_PUSH,       // Push value into generator stack
+    F_G_IF_N,       // Generate condition for if let ID
     F_G_IF_S,       // Generate start of if
     F_G_IF_EL,      // Generate end of if, start of else
     F_G_IF_E,       // Generate end of else
